0a9f255dac115d2d4455ccfe3c71d9d3180d870b
Shashant <shashantchaudhary999@gmail.com>
2023-12-12 19:57:49 +0530

    temp

---
+++ JoePizaa/Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phase3JenkinsProj.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Pizza Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Price")]
+        public float Price { get; set; }
+    }
+}
